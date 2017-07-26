584880074d45ed4576f8d124c02f42597ed1e030
Sven Ruppert <sven.ruppert@gmail.com>
2017-07-26 21:08:33 +1200

    v006

---
+++ src/main/java/org/rapidpm/frp/v006/Main.java
@@ -0,0 +1,68 @@
+package org.rapidpm.frp.v006;
+
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ *
+ */
+public class Main {
+
+  @FunctionalInterface
+  public static interface Service extends Function<String, Optional<Integer>> {
+    public default Optional<Integer> doWork(String value) {
+      return apply(value);
+    }
+  }
+
+  static Service service() {
+    return (value) -> {
+      try {
+        return Optional.of(Integer.valueOf(value));
+      } catch (NumberFormatException e) {
+        e.printStackTrace();
+      }
+      return Optional.empty();
+    };
+  }
+
+  public static void main(String[] args) {
+
+    Optional<Integer> apply = service().doWork("1");
+
+    final Integer intA = apply.get();
+
+    if (intA != null) {
+      //do something usefull
+    }
+    else {
+      //do something ???
+    }
+
+    final Integer intB1 = apply.orElseGet(() -> - 1);
+    final Integer intB2 = apply.orElse(- 1);
+    final Integer intB3 = apply.orElseThrow(() -> new RuntimeException("panic ;-)"));
+
+    apply.ifPresent(v -> {
+      System.out.println("v = " + v);
+    });
+
+    //since 9
+    apply.ifPresentOrElse(v -> {
+      System.out.println("v = " + v);
+    } , () -> {
+      System.out.println("value not present");
+    });
+
+    apply.or(() -> Optional.of(Integer.MAX_VALUE));
+
+    final Stream<Integer> stream = apply.stream();
+
+
+
+
+  }
+}
