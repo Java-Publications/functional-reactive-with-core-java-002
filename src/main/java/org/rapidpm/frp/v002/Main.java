78b3f828d10e15b3aa1d93f3e1ad492547bcaafa
Sven Ruppert <sven.ruppert@gmail.com>
2017-07-26 12:59:02 +1200

    v002

---
+++ src/main/java/org/rapidpm/frp/v002/Main.java
@@ -0,0 +1,32 @@
+package org.rapidpm.frp.v002;
+
+/**
+ *
+ */
+public class Main {
+
+  @FunctionalInterface
+  public static interface Service {
+    public Integer doWork(String value);
+  }
+
+  public static class ServiceImpl implements Service {
+    @Override
+    public Integer doWork(String value) {
+      return Integer.valueOf(value);
+    }
+  }
+
+  public static void main(String[] args) {
+    Integer integer = new ServiceImpl().doWork("1");
+
+    Service serviceA = (value)-> { return Integer.valueOf(value);};
+
+    Service serviceB = Integer::valueOf;
+
+    serviceA.doWork("1");
+    serviceB.doWork("1");
+
+  }
+
+}
