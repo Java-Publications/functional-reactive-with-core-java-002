062ed09e4da11d1fd3088332f1144c049e4c668f
Sven Ruppert <sven.ruppert@gmail.com>
2017-07-26 21:38:31 +1200

    v007

---
+++ src/main/java/org/rapidpm/frp/v007/Main.java
@@ -0,0 +1,42 @@
+package org.rapidpm.frp.v007;
+
+
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+/**
+ *
+ */
+public class Main {
+
+  @FunctionalInterface
+  public static interface Service extends Function<String, Optional<Integer>> {
+    public default Optional<Integer> doWork(String value) {
+      return apply(value);
+    }
+  }
+
+  static Service service() {
+    return (value) -> {
+      try {
+        return Optional.of(Integer.valueOf(value));
+      } catch (NumberFormatException e) {
+        e.printStackTrace();
+      }
+      return Optional.empty();
+    };
+  }
+
+  public static void main(String[] args) {
+
+    String value = "";
+    service()
+        .doWork(value)
+        .or(() -> Optional.of(0))  // per definition
+        .map((Function<Integer, Function<Integer, String>>) integer -> (valueToAdd) -> integer + valueToAdd + " was calculated")
+        .ifPresentOrElse(
+            fkt -> System.out.println("f(10) ==> = " + fkt.apply(10)),
+            () -> System.out.println("value not present (usless here)"));
+  }
+}
