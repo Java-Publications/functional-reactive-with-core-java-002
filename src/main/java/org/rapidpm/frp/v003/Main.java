9d47e822fb6ebc916a4d1869ef58a19110bc8851
Sven Ruppert <sven.ruppert@gmail.com>
2017-07-26 13:09:48 +1200

    v003

---
+++ src/main/java/org/rapidpm/frp/v003/Main.java
@@ -0,0 +1,44 @@
+package org.rapidpm.frp.v003;
+
+import java.util.Optional;
+
+/**
+ *
+ */
+public class Main {
+
+  @FunctionalInterface
+  public static interface Service {
+    public Optional<Integer> doWork(String value);
+  }
+
+  public static class ServiceImpl implements Service {
+    @Override
+    public Optional<Integer> doWork(String value) {
+      try {
+        return Optional.of(Integer.valueOf(value));
+      } catch (NumberFormatException e) {
+        e.printStackTrace();
+      }
+      return Optional.empty();
+    }
+  }
+
+  public static void main(String[] args) {
+    Optional<Integer> integer = new ServiceImpl().doWork("1");
+
+    Service serviceA = (value) -> {
+      try {
+        return Optional.of(Integer.valueOf(value));
+      } catch (NumberFormatException e) {
+        e.printStackTrace();
+      }
+      return Optional.empty();
+    };
+
+    serviceA
+         .doWork("1");
+
+  }
+
+}
